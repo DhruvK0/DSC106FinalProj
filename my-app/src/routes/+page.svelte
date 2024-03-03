820dc265795c46cdcb70ec0af99be535e430eec3
Charlie-279 <cgillet@ucsd.edu>
2024-03-02 23:33:36 -0800

    hhhhh

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    // import ScrollyTeller from '../components/ScrollyTeller.svelte'
+    import StackedAreaChart from "../components/StackedAreaChart.svelte";
+</script>
+
+<main>
+    <!-- <ScrollyTeller /> -->
+    <StackedAreaChart />
+</main>
