3063dc2c5899415767994c8ed21cb76393b11561
Charlie-279 <cgillet@ucsd.edu>
2024-03-03 19:08:53 -0800

    Revert "build"
    This reverts commit 1d5a4ae76ecf6c9b8a72b16ed2339365c2f5ec8e.

--- Scrollytelling/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.ee9e1d72.js","_app/immutable/chunks/index.019f7634.js","_app/immutable/chunks/singletons.c0fbb7f7.js"];
-export const stylesheets = [];
-export const fonts = [];
