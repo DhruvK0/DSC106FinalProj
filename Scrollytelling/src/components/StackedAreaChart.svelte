f6c16563b252e6e806520c2488c58f63a5db555d
Charlie-279 <cgillet@ucsd.edu>
2024-03-03 19:08:49 -0800

    Revert "fixing plot"
    This reverts commit aa910eea8dc0df3f691fe4b7b8e77035c0041a35.

--- Scrollytelling/src/components/StackedAreaChart.svelte
+++ Scrollytelling/src/components/StackedAreaChart.svelte
@@ -5,9 +5,9 @@
 
     onMount(() => {
         d3.csv("src/data/patents_grouped.csv").then(function(data) {
-            const margin = { top: 10, right: 30, bottom: 30, left: 30 };
+            const margin = { top: 20, right: 30, bottom: 30, left: 50 };
             const width = 800 - margin.left - margin.right;
-            const height = 500 - margin.top - margin.bottom;
+            const height = 400 - margin.top - margin.bottom;
 
             const svg = d3.select(".chart")
                 .append("svg")
@@ -102,24 +102,6 @@
             svg.append("g")
                 .attr("transform", `translate(${margin.left},0)`)
                 .call(yAxis);
-
-            // Add the x-axis label
-            svg.append("text")
-                .attr("class", "x-axis-label")
-                .attr("x", width / 2)
-                .attr("y", height + margin.top + 20)
-                .attr("text-anchor", "middle")
-                .text("Year");
-
-            // Add the y-axis label
-            svg.append("text")
-                .attr("class", "y-axis-label")
-                .attr("x", -height / 2)
-                .attr("y", -margin.left)
-                .attr("dy", "1em")
-                .attr("transform", "rotate(-90)")
-                .attr("text-anchor", "middle")
-                .text("Percentage of Patents");
 
         }).catch(function(error) {
             console.log(error);
